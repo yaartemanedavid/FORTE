49d1892104c648e4fee5f4be7811996ef4f04531
yaartemanedavid <artemchan21@gmail.com>
2024-12-21 16:23:33 +0300

    Add files via upload

---
+++ engine/fb/events/E_CYCLE.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+
+from engine.desc import FbDesc, OutputDesc, ExecContext, InputDesc
+from utils import parse_duration
+
+logger = logging.getLogger(__name__)
+
+class E_CYCLE(FbDesc):
+    def __init__(self):
+        super().__init__(
+            name='E_CYCLE',
+            inputs=[
+                InputDesc(name='START', input_kind='event'),
+                InputDesc(name='STOP', input_kind='event'),
+                InputDesc(name='DT', input_kind='value'),
+            ],
+            outputs=[
+                OutputDesc(name='EO', output_kind='event'),
+            ],
+        )
+
+    async def exec(self, context: ExecContext, args: dict[str, any]):
+        if context.io_name == 'START':
+            if 'current_task' in context.store: return
+            delay = parse_duration(args['DT'])
+
+            while True:
+                context.trigger('EO')
+                task = asyncio.create_task(asyncio.sleep(delay))
+                context.store['current_task'] = task
+                logger.debug('Sleeping for %s seconds', delay)
+
+                try:
+                    await task
+                except asyncio.CancelledError:
+                    logger.debug('Task cancelled')
+                    return
+
+        if context.io_name == 'STOP':
+            if 'current_task' not in context.store: return
+            context.store['current_task'].cancel()
---
+++ engine/fb/events/__init__.py
@@ -0,0 +1,3 @@
+from .E_CYCLE import E_CYCLE
+from .E_DELAY import E_DELAY
+from .E_RESTART import E_RESTART
