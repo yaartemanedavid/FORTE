c47fdef9a1958eb99d5cb7417d858a021379bfff
yaartemanedavid <artemchan21@gmail.com>
2024-12-21 16:21:51 +0300

    Add files via upload

---
+++ engine/fb/comparison/F_LT.py
@@ -0,0 +1,33 @@
+import logging
+
+from engine.desc import FbDesc, InputDesc, OutputDesc, ExecContext
+
+logger = logging.getLogger(__name__)
+
+class F_LT(FbDesc):
+    def __init__(self):
+        super().__init__(
+            name='F_LT',
+            inputs=[
+                InputDesc('REQ', 'event'),
+                InputDesc('IN1', 'value'),
+                InputDesc('IN2', 'value'),
+            ],
+            outputs=[
+                OutputDesc('CNF', 'event'),
+                OutputDesc('OUT', 'value'),
+            ],
+        )
+
+    async def exec(self, context: ExecContext, args: dict[str, any]):
+        args = [
+            args['IN1'],
+            args['IN2'],
+        ]
+
+        logger.debug('Evaluating expression %s', ' < '.join(map(str, args)))
+        res = args[0] < args[1]
+        logger.debug('Got result: %s', res)
+
+        await context.supply_output('OUT', res)
+        context.trigger('CNF')
---
+++ engine/fb/comparison/__init__.py
@@ -0,0 +1,6 @@
+from .F_EQ import F_EQ
+from .F_GE import F_GE
+from .F_GT import F_GT
+from .F_LE import F_LE
+from .F_LT import F_LT
+from .F_NE import F_NE
